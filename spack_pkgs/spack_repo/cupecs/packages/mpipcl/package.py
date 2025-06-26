68edf3c57c65e5fbb8553ab543c151f8a8fd4d88
aworley16 <apworley@gmail.com>
2025-06-26 10:22:19 -0500

    fixed error in mpipcl package URL

--- spack_pkgs/spack_repo/cupecs/packages/mpipcl/package.py
+++ spack_pkgs/spack_repo/cupecs/packages/mpipcl/package.py
@@ -9,12 +9,21 @@ from spack.package import *
 
 
 class Mpipcl(CMakePackage):
+    """An open source shared library that implements the partitioned communication interface approved in MPI 4.0
 
+        mpipcl is a part of the MPI-Advance Project developed and maintained by CUP-ECS.
+    """
+
+    homepage = "https://github.com/mpi-advance/MPIPCL.git"
+    url = "https://github.com/mpi-advance/MPIPCL/archive/refs/tags/v1.4.0.tar.gz"
+    git = "https://github.com/mpi-advance/MPIPCL.git"
+
+    maintainers("aworley16")
     license("BSD-3-Clause", checked_by="aworley16")
 
     version("testing", branch="spack-target")
 
-    version("1.4.0", sha256="dad7c458aadcd68d59f96485288384d21f25b1b988efb53af686ef7a8a59a6ce")
+    version("1.4.0", tag='v1.4.0')
     version("1.3.0", sha256="ad65f5b87e57530927d1607fd7856f8d29bc34d4198490d34a70edefcd16f02a")
     version("1.2.0", sha256="6d28fdb452ed75a6c5623ac684aed39a71592524afca5804b39102d7a46bb6b8")
     version("1.1.2", sha256="37e842b0a1733df7e88df47cfbe95064077befbcc8bdfeb9fc0a9014c6e38a47")
