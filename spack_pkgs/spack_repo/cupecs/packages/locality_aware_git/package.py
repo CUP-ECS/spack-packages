92ae688dc03f5b4b7b1b693933bc1493f1b84594
aworley16 <apworley@gmail.com>
2025-10-23 16:46:30 -0500

    updated package to find and use cuda_arch

--- spack_pkgs/spack_repo/cupecs/packages/locality_aware_git/package.py
+++ spack_pkgs/spack_repo/cupecs/packages/locality_aware_git/package.py
@@ -10,11 +10,11 @@
 # next to all the things you'll want to change. Once you've handled
 # them, you can save this file and test your package like this:
 #
-#     spack install mpiadvance
+#     spack install locality-aware-git
 #
 # You can edit this file again by typing:
 #
-#     spack edit mpiadvance
+#     spack edit locality-aware-git
 #
 # See the Spack documentation for more information on packaging.
 # ----------------------------------------------------------------------------
@@ -22,37 +22,34 @@
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
+from spack_repo.builtin.build_systems.generic import Package
 
 from spack.package import *
 
 
-class Mpiadvance(CMakePackage, CudaPackage, ROCmPackage):
+class LocalityAwareGit(CMakePackage, CudaPackage, ROCmPackage):
+    """FIXME: Put a proper description of your package here."""
     """Locality-aware optimizations for standard MPI collectives as well as neighborhood collectives."""
 
-    homepage = "https://github.com/mpi-advance"
-    url =" https://github.com/mpi-advance/mpi-advance.git"
-    git = "git@github.com:mpi-advance/mpi-advance.git"
-
-    maintainers("aworley16", "JStewart28")
+    git = "git@github.com:aworley16/locality_aware.git"
+    branch = "experimental"
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers("github_user1", "github_user2")
+    maintainers("bienz2", "JStewart28")
 
     license("BSD-3-Clause")
-    
-    version("test", branch="experimental", submodules=True)
+
+    version("1.0", commit="ecfb55c159c5ee6cdc93d1c039d443368fae5ab7")
+    version("master", branch="master")
     
     depends_on("c", type="build")
     depends_on("cxx", type="build")
-    
+
     # Variants are primarily backends to build on GPU systems and pass the right
     # informtion to the packages we depend on
-    variant("cuda", default=False, description="Use CUDA support from subpackages")
-    variant("openmp", default=False, description="Use OpenMP support from subpackages")
-    
-    # varients for disabling various sublibraries
-    variant("pcl", default=True, description="Build MPIPCL library")
-    variant("st",  default=True, description="Build Stream-triggering library")
-    variant("la",  default=True, description="Build Stream-triggering library")
-    variant("tests", default=False, description="Build examples and test programs")
-    # variant("+cuda", default=True, description="Build MPIPCL library")
+    # variant("cuda", default=False, description="Use CUDA support from subpackages")
+    # variant("openmp", default=False, description="Use OpenMP support from subpackages")
 
     # MPI dependencies
     depends_on("mpi")
@@ -76,69 +73,20 @@ class Mpiadvance(CMakePackage, CudaPackage, ROCmPackage):
     conflicts("openmpi ~cuda", when="+cuda")
     conflicts("^intel-mpi")  # Heffte won't build with intel MPI because of needed C++ MPI support
     # Commenting so we can test C++20 and cuda@12.2.1 on Lassen
+    # Commenting so we can test C++20 and cuda@12.2.1 on Lassen
     # conflicts("^spectrum-mpi", when="^cuda@11.3:") # cuda-aware spectrum is broken with cuda 11.3:
+
 
     # CMake specific build functions
     def cmake_args(self):
         args = []
-        print("!!--!!")
-        args.append("-DSPACK=ON")
-        #Parse libraries NOT to be installed. 
-        if self.spec.satisfies("+pcl"):
-            print("PC SET")
-            args.append("-DMPIA_PC=ON")
-        else:
-            print("PC NOT SET")
-            
-        if self.spec.satisfies("+st"):
-            print("ST SET")
-            args.append("-DMPIA_ST=ON")
-            
-        else:
-            print("ST NOT SET") 
-            
-        if self.spec.satisfies("+la"):
-            print("LA SET")
-            args.append("-DMPIA_LA=ON")
-            
-            if self.spec.satisfies("+cuda"):
-                args.append("-DUSE_CUDA=ON")
-                print(self.spec.variants["cuda_arch"].value)
-                mystring = ', '.join(map(str, self.spec.variants["cuda_arch"].value)) 
-                print(mystring)
-                cuda_val = "-DCMAKE_CUDA_ARCHITECTURES=" + mystring
-                print(cuda_val)
-                args.append(cuda_val)
-                
-        else:
-            print("LA NOT SET")   
-        
-        #add in tests if requested
-        if self.spec.satisfies("+tests"):
-            print("TESTS TO BE BUILT")
-            args.append("-DBUILD_EXAMPLES=ON")
-            args.append("-DENABLE_UNIT_TESTS=ON")
-        args.append("-DSPACK=ON")
-        
-        # If Cuda add flags to activate cuda build options
-        if self.spec.satisfies("+cuda"):
-            print("BUILDING CUDA SUPPORT")
-            if self.spec.satisfies("la"):
-                args.append("-DUSE_CUDA=ON")
-                args.append("-DCMAKE_CUDA_ARCHITECTURES=\"75\"")
-            if self.spec.satisfies("st"):\
-                args.append("-DUSE_CUDA_BACKEND=ON")
-          
-        
+
         # Use hipcc as the c compiler if we are compiling for rocm. Doing it this way
         # keeps the wrapper insted of changeing CMAKE_CXX_COMPILER keeps the spack wrapper
         # and the rpaths it sets for us from the underlying spec.
         if self.spec.satisfies("+rocm"):
             env["SPACK_CXX"] = self.spec["hip"].hipcc
-            if self.spec.satisfies("la"):
-                args.append("-DUSE_HIP=ON")
-            if self.spec.satisfies("st"):
-                args.append("-DUSE_HIP_BACKEND=ON")
+
         # If we're building with cray mpich, we need to make sure we get the GTL library for
         # gpu-aware MPI
         if self.spec.satisfies("+rocm ^cray-mpich"):
@@ -151,15 +99,4 @@ class Mpiadvance(CMakePackage, CudaPackage, ROCmPackage):
             args.append(
                 "-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath={0} -L{0} -lmpi_gtl_cuda".format(gtl_dir)
             )
-        
-       
-        
         return args
-
-    @run_after('install')
-    @on_package_attributes(test=True) # Run only when --test is provided
-    def check(self):
-        """Run CTest tests."""
-        with working_dir(self.build_directory):
-            ctest_command = ["ctest", "--output-on-failure"]
-            self.run_command(ctest_command)
